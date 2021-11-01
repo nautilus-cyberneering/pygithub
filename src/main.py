823c06adacb88284c4b5ccd1c4cf43427f68381b
Jose Celano <josecelano@gmail.com>
2021-11-01 16:41:00 +0000

    update image with the same content using GitHub API

--- src/main.py
+++ src/main.py
@@ -10,27 +10,43 @@ from github import Github
 def main(repo_token):
     gh = Github(repo_token)
 
-    repository = "Nautilus-Cyberneering/chinese-ideographs-website"
+    repository = "josecelano/pygithub"
 
     remote_repo = gh.get_repo(repository)
 
-    commit = remote_repo.get_commit("79c3511c1eba9329665dc64c1bd55ff4cffeb98b")
+    # Get commit
+
+    commit = remote_repo.get_commit("03aaa45f46b313ed6079cd2e8788173bd0a3af52")
 
     pprint(commit)
+
+    # Get issue
 
     issue = remote_repo.get_issue(number=1)
 
     pprint(issue)
 
+    # Get file content
+
     # 1 MB limit
-    issue = remote_repo.get_contents(
-        "public/images/.gitkeep", "issue-1-import-base-images")
+    issue = remote_repo.get_contents("data/.gitkeep", "main")
+
+    # Calculate file hash
 
     local_repo = Repo("/app")
 
     sha = local_repo.git.hash_object('data/000001-42.600.2.tif')
 
     print("sha for data/000001-42.600.2.tif (git hash-object data/000001-42.600.2.tif)", sha)
+
+    # Update binary file. Update data/000002-42.600.2.tif with the same content
+
+    branch = "main"
+    commit_message = f'Update file data/000001-42.600.2.tif'
+    content = open("data/000001-42.600.2.tif", "rb").read()
+    content_base64 = base64.encodebytes(content)
+    response = remote_repo.update_file(
+        "000002-42.600.2.tif", commit_message, content, sha, branch)
 
 
 if __name__ == "__main__":
