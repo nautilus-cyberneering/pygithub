a49d6e5c37902773101b261796501b4fad9dba85
Jose Celano <josecelano@gmail.com>
2021-11-01 17:05:00 +0000

    print file info from dir and comment upload file

--- src/main.py
+++ src/main.py
@@ -33,6 +33,16 @@ def main(repo_token):
 
     pprint(file_content)
 
+    # Get dir info
+
+    # 1 MB limit
+    dir_content = remote_repo.get_contents("data", "main")
+
+    for file in dir_content:
+        print("File: ", file.path, " sha:", file.sha)
+
+    pprint(dir_content)    
+
     # Calculate file hash
 
     local_repo = Repo("/app")
@@ -43,12 +53,12 @@ def main(repo_token):
 
     # Update binary file. Update data/000002-42.600.2.tif with the same content
 
-    branch = "main"
-    commit_message = f'Update file data/000001-42.600.2.tif'
-    content = open("data/000001-42.600.2.tif", "rb").read()
-    content_base64 = base64.encodebytes(content)
-    response = remote_repo.update_file(
-        "000002-42.600.2.tif", commit_message, content, sha, branch)
+    # branch = "main"
+    # commit_message = f'Update file data/000001-42.600.2.tif'
+    # content = open("data/000001-42.600.2.tif", "rb").read()
+    # content_base64 = base64.encodebytes(content)
+    # response = remote_repo.update_file(
+    #     "000002-42.600.2.tif", commit_message, content, sha, branch)
 
 
 if __name__ == "__main__":
